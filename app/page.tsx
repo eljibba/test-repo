6c18da9dcebd76b987a7abae927af06f62926c9c
el jibba <afilehit@gmail.com>
2023-08-09 12:16:42 +0900

    2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import styles from './page.module.css';
 
 export default function Home() {
-  console.log(123);
   return (
     <main className={styles.main}>
       <h1 className={styles.post_header}>.my_blog</h1>
