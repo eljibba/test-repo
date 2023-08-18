66606ed5635cfdbec17af5407b0d1cc47a0265ae
el jibba <afilehit@gmail.com>
2023-08-18 14:16:48 +0900

    rating component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import { Button, Htag, P } from './components';
+'use client';
+
+import { useState } from 'react';
+
+import { Button, Htag, P, Rating } from './components';
 import styles from './page.module.css';
 import Logo from '../public/vercel.svg';
 import { Tag } from './components/Tag/Tag';
 
 export default function Home() {
+  const [rating, setRating] = useState<number>(4);
+  
   return (
     <main className={styles.main}>
       <Logo />
@@ -18,6 +24,8 @@ export default function Home() {
       <Tag href='#' size='sm'>Маленький</Tag>
       <Tag size='base' color='gray'>Обычный</Tag>
       <Tag size='md' color='primary'>Средний</Tag>
+
+      <Rating rating={rating} setRating={setRating} isEditable />
 
       <Button appearance='primary' arrow='down'>Кнопка</Button>
       <Button appearance='ghost' arrow='right'>Кнопка</Button>
